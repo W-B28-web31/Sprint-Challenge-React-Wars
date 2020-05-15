462ed29cf726c6a5f6ec4b41dcf22e0e8815c79d
William Bell <wtb2804@gmail.com>
2020-05-15 13:06:12 -0400

    New MVP

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,39 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+const Row = styled.div `
+	margin: 0px auto;
+	width: 100%;
+	border-top: .5px grey solid;
+	border-bottom: .5px grey solid;
+`
+
+const Card = styled.div `
+	width: 100%;
+	align-items: center;
+    color: #443e3e;
+    text-shadow: 1px 1px 5px #fff;	margin: 25px 0px;
+	padding: 15px 0px;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+`
+
+const Character = props => {
+    console.log(props)
+    
+    return (
+    <Row>
+        <Card>
+            <h1>{props.data.name}</h1>
+            <h3>Birth Year: {props.data.birth_year}</h3>
+            <h3>Eye Color: {props.data.eye_color}</h3>
+            <h3>Height: {props.data.height}</h3>
+            <h3>Mass: {props.data.mass} units</h3>
+            <h4>This character was in {props.data.films.length} film(s)</h4>
+        </Card>
+    </Row>
+    )
+}
+
+export default Character
